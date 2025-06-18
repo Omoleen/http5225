8dfa7ef8a46869588b1f481dac879c96e785bf7d
Emmanuel Omole <46651988+Omoleen@users.noreply.github.com>
2025-06-18 19:51:24 -0400

    Add Week 7 files for school management system: implement add, delete, detail, and update functionalities with database connection.

--- week6/index.php
+++ week6/index.php
@@ -1,36 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+        $connect = mysqli_connect("localhost", "root", "password", "colors");
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ontario Public Schools</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+        if(!$connect){
+            die( "Connection failed: " . mysqli_connect_error());
+        }
+        $query = 'SELECT * FROM colors';
 
-<body>
-  <h1 class="display-4 mt-5 mb-5">All Schools</h1>
-  <?php
-  require('connect.php');
-  $query = 'SELECT * FROM schools';
-  $schools = mysqli_query($connect, $query);
-  ?>
-  <?php
-  foreach ($schools as $school) {
-    echo $school['School Name'] . '<form action="update.php" method="GET">
-        <input type="hidden" name="id" value="' . $school['id'] . '">
-        <input type="submit" value="EDIT">
-      </form>';
-    echo '<form action="delete.php" method="GET">
-        <input type="hidden" name="id" value="' . $school['id'] . '">
-        <input type="submit" value="DELETE">
-      </form>';
-    echo '<form action="detail.php" method="GET">
-        <input type="hidden" name="id" value="' . $school['id'] . '">
-        <input type="submit" value="DETAILS">
-      </form>';
-  }
-  ?>
-</body>
+        $colors = mysqli_query($connect, $query);
+        // print_r($colors);
 
-</html>
+        if($colors){
+            foreach($colors as $color){
+                // echo $color['Name'];
+                echo '<div class="' . $color['Name'] . '"' . ' style = "background-color: ' . $color['Hex'] . '"' . ' >' . $color['Hex'] . '</div>';
+            }
+        }
+    ?>
