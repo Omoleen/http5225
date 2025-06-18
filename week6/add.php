10af72a070b951e5abd677c15d0920b3bbe1f3b2
Emmanuel Omole <46651988+Omoleen@users.noreply.github.com>
2025-06-18 19:48:54 -0400

    Add Week 6 files for school management system: add, delete, detail, update, and connect functionalities with database integration.

---
+++ week6/add.php
@@ -0,0 +1,38 @@
+<?php
+if (isset($_POST['AddSchool'])) {
+  $BoardName = $_POST['BoardName'];
+  $SchoolName = $_POST['SchoolName'];
+  // print_r($_POST);
+  // echo "Board Name: " . htmlspecialchars($BoardName) . "<br>";
+  // echo "School Name: " . htmlspecialchars($SchoolName) . "<br>";
+  require('connect.php');
+  $sql = "INSERT INTO schools (`Board Name`, `School Name`) VALUES ('$BoardName', '$SchoolName')";
+  
+  $result = mysqli_query($connect, $sql);
+  if($result){
+    header("Location: index.php");
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Add School</title>
+</head>
+
+<body>
+  <h1>Add a School</h1>
+
+  <form action="add.php" method="POST">
+    <input type="text" name="BoardName" placeholder="Board Name">
+    <input type="text" name="SchoolName" placeholder="School Name">
+    <input type="submit" value="Add School" name="AddSchool">
+  </form>
+
+</body>
+
+</html>
